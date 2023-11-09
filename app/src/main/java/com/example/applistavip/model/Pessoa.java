5e29ef7b4e0b2743c6952a68264ae89809455250
thallysYon <thallyayon@gmail.com>
2023-11-09 07:16:51 -0300

    find view by id and clear button "limpar"

--- app/src/main/java/com/example/applistavip/model/Pessoa.java
+++ app/src/main/java/com/example/applistavip/model/Pessoa.java
@@ -1,4 +1,47 @@
 package com.example.applistavip.model;
 
+import androidx.annotation.NonNull;
+
 public class Pessoa {
+
+    private String primeiroNome;
+    private String sobrenome;
+    private String cursoDesejado;
+    private String telefoneContato;
+
+    public Pessoa() {
+    }
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
+    }
+
 }
--- app/src/main/java/com/example/applistavip/view/MainActivity.java
+++ app/src/main/java/com/example/applistavip/view/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.applistavip.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.applistavip.R;
+import com.example.applistavip.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText edtTxtNome = findViewById(R.id.edtTxtNome);
+        EditText edtTxtSobrenome = findViewById(R.id.edtTxtSobrenome);
+        EditText edtTxtCurso = findViewById(R.id.edtTxtCurso);
+        EditText edtTxtTelefone = findViewById(R.id.edtTxtTelefone);
+
+        Button  btnLimpar = findViewById(R.id.btnLimpar);
+        Button  btnSalvar = findViewById(R.id.btnSalvar);
+        Button  btnFinalizar = findViewById(R.id.btnFinalizar);
+
+        btnLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edtTxtNome.setText("");
+                edtTxtSobrenome.setText("");
+                edtTxtCurso.setText("");
+                edtTxtTelefone.setText("");
+            }
+        });
     }
 }
