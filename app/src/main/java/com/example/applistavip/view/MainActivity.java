c0a1ba0c339f6c233538c1b6ab25508acd076373
thallysYon <thallyayon@gmail.com>
2023-11-08 22:28:20 -0300

    MVC architecture view package

--- app/src/main/java/com/example/applistavip/view/MainActivity.java
+++ app/src/main/java/com/example/applistavip/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.applistavip;
+package com.example.applistavip.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.applistavip.R;
 
 public class MainActivity extends AppCompatActivity {
 
